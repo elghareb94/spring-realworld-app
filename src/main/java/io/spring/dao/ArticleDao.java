5cb90f95dfbe5f66fb4d25ad0f60c893b866e2fb
Ahmed Elghareb <elghareb94@gmail.com>
2020-12-29 17:22:57 +0200

    add articleDao and Impl

---
+++ src/main/java/io/spring/dao/ArticleDao.java
@@ -0,0 +1,21 @@
+package io.spring.dao;
+
+import io.spring.entity.Article;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleDao {
+    void save(Article article);
+
+    void update(Article article);
+
+    void delete(Article article);
+
+    Optional<Article> findById(Long id);
+
+    Optional<Article> findBySlug(String slug);
+
+    List<Article> findAll();
+
+}

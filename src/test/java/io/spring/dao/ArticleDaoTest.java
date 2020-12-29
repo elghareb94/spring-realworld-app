ba0df724a174313a923271723f9f279fd57b0a4d
Ahmed Elghareb <elghareb94@gmail.com>
2020-12-29 17:23:10 +0200

    add articleDaoTest

---
+++ src/test/java/io/spring/dao/ArticleDaoTest.java
@@ -0,0 +1,88 @@
+package io.spring.dao;
+
+import io.spring.config.AppConfig;
+import io.spring.config.DataSourceConfig;
+import io.spring.entity.Article;
+import org.hibernate.SessionFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {DataSourceConfig.class, AppConfig.class})
+@ActiveProfiles("test")
+@Transactional
+public class ArticleDaoTest {
+
+    @Autowired
+    private ArticleDao articleDao;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Article article;
+
+    @Before
+    public void setUp() {
+        article = Article.builder().slug("slug").description("Desc").body("body").build();
+        articleDao.save(article);
+        sessionFactory.getCurrentSession().detach(article);
+    }
+
+    @Test
+    public void testFindById() {
+        Optional<Article> optionalArticle = articleDao.findById(article.getId());
+        Assert.assertEquals(article, optionalArticle.get());
+        Assert.assertFalse(article == optionalArticle.get());
+
+    }
+
+    @Test
+    public void testFindBySlug() {
+        Optional<Article> optionalArticle = articleDao.findBySlug(article.getSlug());
+        Assert.assertEquals(article, optionalArticle.get());
+        Assert.assertFalse(article == optionalArticle.get());
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Article> articles = articleDao.findAll();
+        Assert.assertEquals(article, articles.get(0));
+        Assert.assertEquals(articles.size(), 1);
+        Assert.assertFalse(article == articles.get(0));
+    }
+
+    @Test
+    public void testUpdate() {
+        article.setDescription("newDesc");
+        article.setBody("new Body");
+        articleDao.update(article);
+
+        sessionFactory.getCurrentSession().flush();
+        sessionFactory.getCurrentSession().detach(article);
+
+        Optional<Article> optionalArticle = articleDao.findById(article.getId());
+        Assert.assertEquals(article, optionalArticle.get());
+        Assert.assertFalse(article == optionalArticle.get());
+    }
+
+    @Test
+    public void testDelete() {
+        articleDao.delete(article);
+
+        sessionFactory.getCurrentSession().flush();
+        sessionFactory.getCurrentSession().detach(article);
+
+        Optional<Article> optionalArticle = articleDao.findById(article.getId());
+        Assert.assertFalse(optionalArticle.isPresent());
+    }
+}

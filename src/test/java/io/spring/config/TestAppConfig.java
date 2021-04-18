d124db1d29efc34b5ad904b6e10003bc8d286dcd
Ahmed Elghareb <elghareb94@gmail.com>
2021-04-18 14:51:37 +0200

    test ci with commit regarding test

---
+++ src/test/java/io/spring/config/TestAppConfig.java
@@ -0,0 +1,66 @@
+package io.spring.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@PropertySource({"classpath:db/application-test.properties"})
+@ComponentScan({"io.spring"})
+@EnableTransactionManagement
+@Configuration
+@Profile("test")
+public class TestAppConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(additionalProperties());
+
+        return sessionFactory;
+    }
+
+
+    Properties additionalProperties() {
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        props.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
+
+    //for EnableTransactionManagement
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
+
+    @Bean
+    public DataSource h2DataSource() {
+
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+}
